92ad87347e03dbf2382b6e4b2a74d07ed808c050
Effie <effie.thedev@gmail.com>
2025-01-24 15:11:47 +0900

    [feature] 컬러 타입 추가

---
+++ CineHive/CineHive/Resources/CHColor.swift
@@ -0,0 +1,16 @@
+//
+//  CHColor.swift
+//  CineHive
+//
+//  Created by Effie on 1/24/25.
+//
+
+import UIKit
+
+enum CHColor {
+    static let darkLabelBackground = UIColor(named: "dark-label-background")
+    static let lightLabelBackground = UIColor(named: "light-label-background")
+    static let mainBackground = UIColor(named: "main-background")
+    static let theme = UIColor(named: "hive-theme")
+    static let primaryText = UIColor(named: "primary-text")
+}
