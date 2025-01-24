0f003a61791264718f506414edc36abed109e09d
Effie <effie.thedev@gmail.com>
2025-01-24 15:15:07 +0900

    [feature] 이미지 관련 타입 추가

---
+++ CineHive/CineHive/Resources/CHImage.swift
@@ -0,0 +1,13 @@
+//
+//  CHImage.swift
+//  CineHive
+//
+//  Created by Effie on 1/24/25.
+//
+
+import UIKit
+
+enum CHImageName {
+    static let onboarding = "onboarding"
+    static let profilePrefix = "profile_"
+}
