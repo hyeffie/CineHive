edd33020bd5f4a765abaca42b950e5d4d37d2a8a
Effie <effie.thedev@gmail.com>
2025-01-24 14:26:41 +0900

    [config] 프로젝트 생성 및 초기 설정

---
+++ CineHive/CineHive/App/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  CineHive
+//
+//  Created by Effie on 1/24/25.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = viewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+    
+    private func viewController() -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .green
+        return viewController
+    }
+}
