1bd9198c4baf023a35300a237307214ca6969c1f
Effie <effie.thedev@gmail.com>
2025-01-24 21:10:19 +0900

    NicknameTextField 구현

---
+++ CineHive/CineHive/Presentation/Profile/View/NicknameTextField.swift
@@ -0,0 +1,38 @@
+//
+//  NicknameTextField.swift
+//  CineHive
+//
+//  Created by Effie on 1/24/25.
+//
+
+import UIKit
+
+final class NicknameTextField: UITextField {
+    private let underline = {
+        let view = UIView()
+        view.backgroundColor = CHColor.primaryText.value
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        self.addSubview(self.underline)
+        self.underline.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.05)
+        }
+        
+        self.borderStyle = .none
+        self.textColor = CHColor.primaryText.value
+        self.backgroundColor = .clear
+    }
+}
