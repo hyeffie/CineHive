0ffc5bd068d6372ec78643dabfdbf6d4c0b3586e
Effie <effie.thedev@gmail.com>
2025-01-28 20:08:04 +0900

    [feature] 프로필 이미지 설정 화면 구현

---
+++ CineHive/CineHive/Presentation/Profile/ViewController/ProfileImageViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ProfileImageViewController.swift
+//  CineHive
+//
+//  Created by Effie on 1/28/25.
+//
+
+import UIKit
+
+final class ProfileImageViewController: BaseViewController {
+    private var selectedImageNumber: Int
+    
+    private let imageNumberRange = (0..<12)
+
+    private lazy var selectedProfileImageView = SelectedProfileImageView(imageName: CHImageName.profileImage(number: self.selectedImageNumber))
+    
+    private lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: self.collectionViewLayout()
+    )
+    
+    init(selectedImageNumber: Int) {
+        self.selectedImageNumber = selectedImageNumber
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "프로필 이미지 설정"
+        
+        configureView()
+        
+        let selectedIndexPath = IndexPath(item: self.selectedImageNumber, section: 0)
+        self.collectionView.selectItem(
+            at: selectedIndexPath,
+            animated: false,
+            scrollPosition: .top
+        )
+    }
+}
+
+// MARK: - Configuring Views
+extension ProfileImageViewController {
+    private func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let hInset: CGFloat = 16
+        let numberOfItemsInLine: CGFloat = 4
+        let spacing: CGFloat = 12
+        let itemWidth = (self.view.frame.width - 2 * hInset - (numberOfItemsInLine - 1) * spacing) / numberOfItemsInLine
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.itemSize = .init(width: itemWidth, height: itemWidth)
+        return layout
+    }
+    
+    private func configureView() {
+        let vOffset = 30
+        let hInset = 16
+        
+        self.view.addSubview(self.selectedProfileImageView)
+        self.selectedProfileImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(vOffset)
+            make.centerX.equalTo(self.view.safeAreaLayoutGuide)
+            make.width.equalTo(self.view.safeAreaLayoutGuide).multipliedBy(0.25)
+        }
+        
+        self.view.addSubview(self.collectionView)
+        self.collectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.selectedProfileImageView.snp.bottom).offset(vOffset)
+            make.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide).inset(hInset)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        self.collectionView.registerCellClass(ProfileImageCollectionViewCell.self)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.isScrollEnabled = false
+        self.collectionView.backgroundColor = .clear
+    }
+}
+
+extension ProfileImageViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return imageNumberRange.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell: ProfileImageCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath) else {
+            return UICollectionViewCell()
+        }
+        cell.configureImage(imageNumber: indexPath.item)
+        return cell
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        let number = indexPath.item
+        selectImage(number: number)
+    }
+    
+    private func selectImage(number: Int) {
+        self.selectedImageNumber = number
+        self.selectedProfileImageView.configureImage(number: number)
+    }
+}
+
+#Preview {
+    let vc = ProfileImageViewController(selectedImageNumber: 1)
+    return NavigationController(rootViewController: vc)
+}
