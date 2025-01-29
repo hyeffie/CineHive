ed191dfcb86181d5e936f613a0f078db079cba6f
Effie <effie.thedev@gmail.com>
2025-01-29 21:42:35 +0900

    [feature] 프로필 설정 시 화면 전환 로직 구현 + 코드 정리

--- CineHive/CineHive/Presentation/Profile/ViewController/ProfileEditViewController.swift
+++ CineHive/CineHive/Presentation/Profile/ViewController/ProfileEditViewController.swift
@@ -31,7 +31,7 @@ final class ProfileEditViewController: BaseViewController {
     
     private lazy var completeButton = {
         let button = ThemePillShapeButton(title: "완료")
-        let action = UIAction { [weak self] _ in self?.signUp() }
+        let action = UIAction { [weak self] _ in self?.saveProfile() }
         button.addAction(action, for: .touchUpInside)
         return button
     }()
@@ -39,41 +39,7 @@ final class ProfileEditViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
-        
         setForm()
-    }
-    
-    private func goToProfileImageSelectScene() {
-        let viewController = ProfileImageViewController(
-            selectedImageNumber: self.form.imageNumber,
-            imageSelectionHandler: { [weak self] number in self?.setImage(number: number) }
-        )
-        self.push(viewController)
-    }
-    
-    private func signUp() {
-        let isSignUp = self.userProfile == nil
-        
-        // 현재 입력된 정보 validation
-        validateNickname(self.form.nickname)
-        
-        // 새로운 user profile 생성
-        let newUserProfile = ProfileInfo(
-            imageNumber: self.form.imageNumber,
-            nickname: self.form.nickname,
-            createdAt: self.userProfile?.createdAt ?? .now,
-            likedMovieIDs: self.userProfile?.likedMovieIDs ?? []
-        )
-        
-        // 현재 입력된 정보 user defaults 에 저장
-        self.userProfile = newUserProfile
-        
-        // 화면 전환
-        if isSignUp {
-            // TODO: 탭바 컨트롤러로 교체
-        } else {
-            self.dismiss(animated: true)
-        }
     }
 }
 
@@ -96,16 +62,7 @@ extension ProfileEditViewController {
             make.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide).inset(hInset)
         }
         
-        if let userProfile {
-            self.title = "프로필 편집"
-            let dismissAction = UIAction { [weak self] _ in self?.dismiss(animated: true) }
-            let dismissButton = UIBarButtonItem(image: CHSymbol.xmark.value, primaryAction: dismissAction)
-            self.navigationItem.setLeftBarButton(dismissButton, animated: false)
-            
-            let action = UIAction { [weak self] _ in self?.signUp() }
-            let saveButton = UIBarButtonItem(title: "저장", primaryAction: action)
-            self.navigationItem.setRightBarButton(saveButton, animated: false)
-        } else {
+        if userProfile == nil {
             self.title = "프로필 설정"
             
             self.view.addSubview(self.completeButton)
@@ -113,24 +70,29 @@ extension ProfileEditViewController {
                 make.top.equalTo(self.nicknameTextField.snp.bottom).offset(vOffset)
                 make.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide).inset(hInset)
             }
+        } else {
+            self.title = "프로필 편집"
+            
+            let dismissAction = UIAction { [weak self] _ in self?.dismiss(animated: true) }
+            let dismissButton = UIBarButtonItem(image: CHSymbol.xmark.value, primaryAction: dismissAction)
+            self.navigationItem.setLeftBarButton(dismissButton, animated: false)
+            
+            let action = UIAction { [weak self] _ in self?.saveProfile() }
+            let saveButton = UIBarButtonItem(title: "저장", primaryAction: action)
+            self.navigationItem.setRightBarButton(saveButton, animated: false)
         }
         
         self.nicknameTextField.setActionToTextField(valueChangeHandler: self.validateNickname(_:))
     }
-}
-
-extension ProfileEditViewController {
+    
     private func setForm() {
         self.selectedProfileImageView.configureImage(number: self.form.imageNumber)
         self.nicknameTextField.setNickname(self.form.nickname)
         validateNickname(self.form.nickname)
     }
-    
-    private func setImage(number: Int) {
-        self.form.imageNumber = number
-        self.selectedProfileImageView.configureImage(number: number)
-    }
-    
+}
+
+extension ProfileEditViewController {
     private func validateNickname(_ input: String) {
         let validNicknameLengthRange = (2..<10)
         let invalidCharacterSet = CharacterSet(arrayLiteral: "@", "#", "$", "%")
@@ -154,5 +116,43 @@ extension ProfileEditViewController {
     private func presentValidationResult(_ state: NicknameValidityState) {
         self.nicknameTextField.configureValidationResult(message: state.message)
         self.completeButton.isEnabled = state.isEnabled
+    }
+}
+
+extension ProfileEditViewController {
+    private func goToProfileImageSelectScene() {
+        let viewController = ProfileImageViewController(
+            selectedImageNumber: self.form.imageNumber,
+            imageSelectionHandler: { [weak self] number in self?.setImage(number: number) }
+        )
+        self.push(viewController)
+    }
+    
+    private func setImage(number: Int) {
+        self.form.imageNumber = number
+        self.selectedProfileImageView.configureImage(number: number)
+    }
+}
+
+extension ProfileEditViewController {
+    private func saveProfile() {
+        let isSignUp = self.userProfile == nil
+        
+        validateNickname(self.form.nickname)
+        
+        let newUserProfile = ProfileInfo(
+            imageNumber: self.form.imageNumber,
+            nickname: self.form.nickname,
+            createdAt: self.userProfile?.createdAt ?? .now,
+            likedMovieIDs: self.userProfile?.likedMovieIDs ?? []
+        )
+        
+        self.userProfile = newUserProfile
+        
+        if isSignUp {
+            self.replaceWindowRoot(to: TabBarController())
+        } else {
+            self.dismiss(animated: true)
+        }
     }
 }
