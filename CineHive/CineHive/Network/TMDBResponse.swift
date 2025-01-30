b4d2b6212299b9ac8570ca4f8437551a3bf694a4
Effie <effie.thedev@gmail.com>
2025-01-30 18:12:46 +0900

    [feature] TMDB get 메서드 구현

--- CineHive/CineHive/Network/TMDBResponse.swift
+++ CineHive/CineHive/Network/TMDBResponse.swift
@@ -32,11 +32,3 @@ enum TMDBResponse<SuccessfulResponse: Decodable>: Decodable {
         let success: Bool
     }
 }
-
-/*
- {
-   "status_code": 7,
-   "status_message": "Invalid API key: You must be granted a valid key.",
-   "success": false
- }
- */
