69c81b7827771ad936a14a5889f245f30cf51586
Effie <effie.thedev@gmail.com>
2025-01-30 16:35:17 +0900

    [feature] TMDBResponse 구현

---
+++ CineHive/CineHive/Network/TMDBResponse.swift
@@ -0,0 +1,42 @@
+//
+//  TMDBResponse.swift
+//  CineHive
+//
+//  Created by Effie on 1/30/25.
+//
+
+import Foundation
+
+enum TMDBResponse<SuccessfulResponse: Decodable>: Decodable {
+    case success(SuccessfulResponse)
+    case failure(ErrorResponse)
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let success = try? container.decode(SuccessfulResponse.self) {
+            self = .success(success)
+        } else if let failure = try? container.decode(ErrorResponse.self) {
+            self = .failure(failure)
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "cannot decode to this type"
+            )
+        }
+    }
+    
+    struct ErrorResponse: Decodable {
+        let status_code: Int
+        let status_message: String
+        let success: Bool
+    }
+}
+
+/*
+ {
+   "status_code": 7,
+   "status_message": "Invalid API key: You must be granted a valid key.",
+   "success": false
+ }
+ */
