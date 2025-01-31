57a447573057b7d8c76b6371558fee5673368c40
Effie <effie.thedev@gmail.com>
2025-01-31 22:21:08 +0900

    [UI] 상세 화면 view controller 추가

---
+++ CineHive/CineHive/Presentation/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  CineHive
+//
+//  Created by Effie on 1/31/25.
+//
+
+import UIKit
+
+final class MovieDetailViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
