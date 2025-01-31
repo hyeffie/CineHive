5eda15eb62f364acc1f175c9db8a28ffce37c723
Effie <effie.thedev@gmail.com>
2025-01-31 22:50:21 +0900

    [UI] container view 추가

--- CineHive/CineHive/Presentation/MovieDetail/MovieDetailViewController.swift
+++ CineHive/CineHive/Presentation/MovieDetail/MovieDetailViewController.swift
@@ -11,6 +11,16 @@ final class MovieDetailViewController: BaseViewController {
     
     private let movieDetail: MovieDetail
     
+    private let scrollView = UIScrollView()
+    
+    private let contentStack = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.spacing = 12
+        return stack
+    }()
+    
     init(movieDetail: MovieDetail) {
         self.movieDetail = movieDetail
         super.init(nibName: nil, bundle: nil)
@@ -23,6 +33,29 @@ final class MovieDetailViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
+    }
+    
+    private func configureViews() {
+        self.view.addSubview(self.scrollView)
+        self.scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+            make.width.equalTo(self.view.safeAreaLayoutGuide)
+        }
         
+        self.scrollView.addSubview(self.contentStack)
+        self.contentStack.snp.makeConstraints { make in
+            make.edges.width.equalTo(self.scrollView)
+        }
+        
+        [UIColor.red, .green, .blue].forEach { color in
+            let view = UIView()
+            view.backgroundColor = color
+            self.contentStack.addArrangedSubview(view)
+            
+            view.snp.makeConstraints { make in
+                make.height.equalTo(300)
+            }
+        }
     }
 }
