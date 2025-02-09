52b8db6c34096aed44c3fb73177fdf0a318e59ba
Effie <effie.thedev@gmail.com>
2025-02-10 07:11:13 +0900

    [UI] 버튼 구현

---
+++ CineHive/CineHive/Scene/Profile/View/MBTIElementView.swift
@@ -0,0 +1,111 @@
+//
+//  MBTIElementView.swift
+//  CineHive
+//
+//  Created by Effie on 2/10/25.
+//
+
+import UIKit
+
+struct MBTI {
+    protocol Element { }
+    
+    enum EI: String, Element {
+        case e = "E"
+        case i = "I"
+    }
+    
+    enum NS: String, Element {
+        case n = "N"
+        case s = "S"
+    }
+    
+    enum TF: String, Element {
+        case t = "T"
+        case f = "F"
+    }
+    
+    enum PJ: String, Element {
+        case p = "P"
+        case j = "J"
+    }
+    
+    var ei: EI
+    var ns: NS
+    var tf: TF
+    var pj: PJ
+}
+
+protocol SelectionDelegate: AnyObject {
+    func didSelect()
+    func didDeselect()
+}
+
+final class CharacterButton: UIButton {
+    private weak var selectionDelegate: SelectionDelegate?
+    
+    init(
+        character: Character,
+        selectionDelegate: SelectionDelegate
+    ) {
+        super.init(frame: .zero)
+        configureView(title: "\(character)")
+        addEventHandler()
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if self.isSelected {
+                self.selectionDelegate?.didSelect()
+            } else {
+                self.selectionDelegate?.didDeselect()
+            }
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView(title: String) {
+        self.snp.makeConstraints { make in
+            make.size.equalTo(44)
+        }
+        
+        var configuration = UIButton.Configuration.bordered()
+        
+        var attributeContainer = AttributeContainer()
+        attributeContainer.font = CHFont.largeBold
+        configuration.cornerStyle = .capsule
+        configuration.background.strokeColor = CHColor.lightLabelBackground
+        
+        self.configuration = configuration
+        
+        self.configurationUpdateHandler = { button in
+            var attributeContainer = AttributeContainer()
+            attributeContainer.font = CHFont.large
+            switch button.state {
+            case .normal:
+                attributeContainer.foregroundColor = CHColor.lightLabelBackground
+                
+                button.configuration?.background.strokeWidth = 2
+                button.configuration?.background.backgroundColor = .clear
+            case .selected:
+                attributeContainer.foregroundColor = CHColor.primaryText
+                button.configuration?.background.strokeWidth = 0
+                button.configuration?.background.backgroundColor = CHColor.validCompleteButton
+            default:
+                return
+            }
+            button.configuration?.attributedTitle = AttributedString(title, attributes: attributeContainer)
+        }
+    }
+    
+    private func addEventHandler() {
+        self.addTarget(self, action: #selector(toggle), for: .touchUpInside)
+    }
+    
+    @objc private func toggle() {
+        self.isSelected.toggle()
+    }
+}
